dd00998a736c48bea1a41504a86009a4a7e3051d
Amra Habibovic <ahabibovic1@etf.unsa.ba>
2020-03-22 13:28:29 +0100

    Promjena modula

--- user-service/src/main/java/com/example/userservice/Repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/Repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.example.userservice.Repository;
 
 import com.example.userservice.Entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
