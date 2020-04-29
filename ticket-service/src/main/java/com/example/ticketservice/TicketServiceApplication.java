96fae57bb0bf4c671ebe24ed07fa4d48db815d7e
nhanjalic1 <nhanjalic1@etf.unsa.ba>
2020-04-29 12:35:38 +0200

    Primjer grpc

--- ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
@@ -1,33 +1,43 @@
 package com.example.ticketservice;
 
-
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.example.grpc.HelloRequest;
+import org.example.grpc.HelloResponse;
+import org.example.grpc.HelloServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-
-import java.util.List;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 public class TicketServiceApplication {
+
+
+
+
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
 	public static void main(String[] args) {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",8080 ).usePlaintext().build();
+
+		HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+		System.out.println(helloResponse);
+		channel.shutdown();
 		SpringApplication.run(TicketServiceApplication.class, args);
 	}
+
 
 	/*@Bean
 	public CommandLineRunner demo(STicketRepository srepository, MTicketRepository mrepository, MTicketRouteRepository mrrepository) {
