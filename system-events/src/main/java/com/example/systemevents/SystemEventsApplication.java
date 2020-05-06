cae6b2873060979a9f3e807a616f5e927ab1233e
ahabibovic1 <ahabibovic1@etf.unsa.ba>
2020-05-06 03:31:55 +0200

    Eureka service discovery

--- system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
+++ system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
@@ -1,13 +1,27 @@
 package com.example.systemevents;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.io.IOException;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SystemEventsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(SystemEventsApplication.class, args);
+
+		Server server = ServerBuilder
+				.forPort(9090)
+				.addService(new SystemEventsServiceImpl()).build();
+
+		server.start();
+		System.out.println("Server started");
+		server.awaitTermination();
 	}
 
 
