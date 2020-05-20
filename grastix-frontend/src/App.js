bd37b5a43e25ce238f2275b7b694daafc9f9680d
nhanjalic1 <nhanjalic1@etf.unsa.ba>
2020-05-20 14:34:27 +0200

    Dodan frontend za single tickets usera

--- grastix-frontend/src/App.js
+++ grastix-frontend/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SingleTicket from './Components/MyTickets'
+import MyTickets from './Components/MyTickets';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GrasTix</h1>
     </div>
   );
 }
