0e0453ba1371869546cdf6ded0a283daa5ab4724
ahabibovic1 <ahabibovic1@etf.unsa.ba>
2020-05-31 00:35:52 +0200

    Fix

--- frontend/src/components/main/Header.js
+++ frontend/src/components/main/Header.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
-import { Navbar } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap";
+import logo from "./logo.png";
 
 class Header extends Component {
     render() {
         return (
             <div>
                 <Navbar bg="primary" variant="dark">
+                    <img src={logo} alt="logo" style={{ marginRight: "10px", width: "40px" }} />
                     <Navbar.Brand>GRAStix</Navbar.Brand>
                 </Navbar>
             </div>
-        )
+        );
     }
 }
 
 export default Header;
-
-
-
