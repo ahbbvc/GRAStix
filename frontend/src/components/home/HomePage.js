5cb6276d2af335d01cad8b931b0940292eb006a6
selmav <37186840+selmav@users.noreply.github.com>
2020-06-02 00:37:39 +0200

    Frontend - Login (#32)

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -2,15 +2,43 @@ import React, { Component } from "react";
 import "./HomePage.css";
 import Login from "./Login.js";
 import Register from "./Register.js";
+import axios from "axios";
 
 class HomePage extends Component {
     state = {
+        accessToken: "",
+        refreshToken: ""
     };
+
+    markLogin = (loginRes, userEmail) => {
+        this.setState({
+            accessToken: loginRes.data.access_token,
+            refreshToken: loginRes.data.refresh_token
+        });
+
+        axios.get(`http://localhost:8762/users/user?email=${userEmail}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${this.state.accessToken}`
+                }
+            }
+        ).then((response) => {
+            this.setState({ user: response.data });
+            if (this.state.user.status === 'admin') this.props.history.push('/admin');
+            else this.props.history.push('/tickets');
+        }).catch((error) => {
+            this.setState({
+                alertMessage: "There was an error while processing request.",
+                alertVisible: true,
+                alertColor: "danger",
+            });
+        });
+    }
 
     render() {
         return (
             <div className="home-container">
-                <Login></Login>
+                <Login markLogin={this.markLogin}></Login>
                 <Register></Register>
             </div>
         );
