a8e88ad836567221a55045109f00e7a37d33e5c7
selmav <svucijak1@etf.unsa.ba>
2020-06-06 01:04:34 +0200

    Guards fix & logout

--- frontend/src/components/main/Header.js
+++ frontend/src/components/main/Header.js
@@ -3,12 +3,21 @@ import { Navbar } from "react-bootstrap";
 import logo from "./logo.png";
 
 class Header extends Component {
+    logout = () => {
+        localStorage.removeItem("access_token");
+    }
+
     render() {
         return (
             <div>
-                <Navbar bg="primary" variant="dark">
-                    <img src={logo} alt="logo" style={{ marginRight: "10px", width: "40px" }} />
-                    <Navbar.Brand>GRAStix</Navbar.Brand>
+                <Navbar bg="primary" variant="dark" style={{justifyContent: "space-between"}}>
+                    <div>
+                        <img src={logo} alt="logo" style={{ marginRight: "10px", width: "40px" }} />
+                        <Navbar.Brand>GRAStix</Navbar.Brand>
+                    </div>
+                    <Navbar.Text>
+                        <a href="/" onClick={this.logout}>Log Out</a>
+                    </Navbar.Text>
                 </Navbar>
             </div>
         );
