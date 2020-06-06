904d3cfb8bfeffacc4439eac68728e67b47151d7
selmav <37186840+selmav@users.noreply.github.com>
2020-06-07 01:35:40 +0200

    Interceptor for system-events request on user service (#42)

---
+++ user-service/src/main/java/com/example/userservice/Interceptor/UserServiceInterceptorAppConfig.java
@@ -0,0 +1,17 @@
+package com.example.userservice.Interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class UserServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    UserServiceInterceptor userServiceInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userServiceInterceptor);
+    }
+}
